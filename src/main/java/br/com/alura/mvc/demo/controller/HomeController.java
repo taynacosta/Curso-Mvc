fe7bf85e4461070c30a3b782af19c256d13a44c1
taynacosta <taynamoreira1811@gmail.com>
2021-10-11 19:29:52 -0300

    primeiro commit, conectando com o banco de dados mysql

---
+++ src/main/java/br/com/alura/mvc/demo/controller/HomeController.java
@@ -0,0 +1,33 @@
+package br.com.alura.mvc.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.demo.model.Pedido;
+
+@Controller
+public class HomeController {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		
+	Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+	List <Pedido> pedidos = query.getResultList();
+		
+		model.addAttribute("pedidos", pedidos);
+		
+		return "home";
+	}
+}
---
+++ src/main/java/br/com/alura/mvc/demo/model/Pedido.java
@@ -0,0 +1,69 @@
+package br.com.alura.mvc.demo.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+	private String urlDoProduto;
+	private String urlDaImagem;
+	private String descricao;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+	public LocalDate getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+	public void setDataDaEntrega(LocalDate dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+	public String getUrlDoProduto() {
+		return urlDoProduto;
+	}
+	public void setUrlDoProduto(String urlDoProduto) {
+		this.urlDoProduto = urlDoProduto;
+	}
+	
+	public String getUrlDaImagem() {
+		return urlDaImagem;
+	}
+	public void setUrlDaImagem(String urlDaImagem) {
+		this.urlDaImagem = urlDaImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}

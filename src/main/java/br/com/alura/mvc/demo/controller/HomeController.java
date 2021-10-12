d2ca80b52118539dc50ecc02e1fa335cc2c361ab
taynacosta <taynamoreira1811@gmail.com>
2021-10-12 07:38:01 -0300

    adicionando repository, e iniciando tela de pedidos

--- src/main/java/br/com/alura/mvc/demo/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/demo/controller/HomeController.java
@@ -1,33 +1,28 @@
 package br.com.alura.mvc.demo.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.demo.model.Pedido;
+import br.com.alura.mvc.demo.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
-
-	@PersistenceContext
-	private EntityManager entityManager;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
 		Pedido pedido = new Pedido();
 		
-	Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-	List <Pedido> pedidos = query.getResultList();
-		
+		List <Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
 		
-		return "home";
-	}
+		return "home";	
+	}	
 }
